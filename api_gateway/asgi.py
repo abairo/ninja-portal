db302d1f61828498603ac4d942e9d9d1b70dab0d
Anderson Bairo <anderson.bairo@yahoo.com.br>
2025-04-23 22:49:32 -0300

    Configure static support for admin panel

--- api_gateway/asgi.py
+++ api_gateway/asgi.py
@@ -14,3 +14,10 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api_gateway.settings")
 
 application = get_asgi_application()
+
+
+# TODO evaluate this configuration, maybe we should remove this and configure nginx to serve these static files
+from django.conf import settings
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
+
+application = ASGIStaticFilesHandler(application)
