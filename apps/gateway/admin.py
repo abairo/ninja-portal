afc24f8d649f896b804dbb53b6656717a8c03b83
Anderson Bairo <anderson.bairo@yahoo.com.br>
2025-04-24 23:58:36 -0300

    Configure the admin page (URIPattern):
    - Customize display (list, filter and search fields)
    - Override save_model method to update settings.URI_PATTERNS

--- apps/gateway/admin.py
+++ apps/gateway/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.conf import settings
+from apps.gateway.models import URIPattern
+from .update_patterns import update_patterns
 
-# Register your models here.
+
+class URIPatternAdmin(admin.ModelAdmin):
+    search_fields = ("pattern",)
+    list_display = ("id", "pattern", "methods", "requires_auth")
+    list_filter = ("methods", "requires_auth")
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        settings.URI_PATTERNS = update_patterns()
+
+
+admin.site.register(URIPattern, URIPatternAdmin)
